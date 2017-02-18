f373387a264a146c60c64401b7731988abdfd921
kmclanahan <kmclanahan@berkeley.edu>
2017-02-18 22:35:13 +0000

    adding csv and file

--- dbmgmt/convert_to_csv.py
+++ dbmgmt/convert_to_csv.py
@@ -1,31 +1,29 @@
 import psycopg2
+import sys
+
+filename = sys.argv[1].strip()
+
+print "Writing database to %s" %filename
 
 conn = psycopg2.connect(database="travel_info", user="postgres", password="pass1234", host="localhost", port="5432")
 cur = conn.cursor()
 
-cur.execute("SELECT country, language FROM Twitter_Data")
+query = "SELECT * FROM Twitter_Data"
 
-countries = {}
-languages = {}
+cur.execute(query)
+
 count = 0
 all_data = cur.fetchall()
 
-for data in all_data:
-    countries.setdefault(data[0], 0)
-    countries[data[0]] += 1
+with open(filename, 'w') as outfile:
+    for data in all_data:
 
-    languages.setdefault(data[1], 0)
-    languages[data[1]] += 1
+        outfile.write('"""%s""","""%s""","""%s""","""%s""","""%s""","""%s""","""%s"""' 
+                    %(data[0], data[1], data[2], data[3], data[4], data[5], data[6]) )
 
-    count += 1
+        count += 1
 
 conn.commit()
 conn.close()
 
-for country in countries:
-    print "%s:\t%s" %(country, countries[country])
-
-for language in languages:
-    print "%s\t%s" %(language, languages[language])
-
-print("\nTotal Tweets Logged: %s" %count)
+print "\nWrote %s Tweets to %s" %(count, filename)
