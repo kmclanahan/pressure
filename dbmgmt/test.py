fd420940f7bd46a15014a190e2dca9d4febedb4b
kmclanahan <kmclanahan@ischool.berkeley.edu>
2017-03-09 21:20:47 -0700

    Update test.py

--- dbmgmt/test.py
+++ dbmgmt/test.py
@@ -1,2 +1,10 @@
-word = 'home'
-print "SELECT country, tweet FROM Twitter_Data WHERE tweet LIKE '%%%s%%'" %word
+import psycopg2
+
+conn = psycopg2.connect(database="travel_info", user="postgres", password="pass1234", host="localhost", port="5432")
+
+cur = conn.cursor()
+
+cur.execute("INSERT INTO Twitter_Data (ts, tweet, location, country, language, coordinates, threat_type) VALUES ( now(), %s, %s, %s, %s, %s, %s)", (tweet, location, country, language, coords, '0'))
+
+conn.commit()
+conn.close()
