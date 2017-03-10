e0a18a299b2f7674266b2a4ec122130e30953497
kmclanahan <kmclanahan@ischool.berkeley.edu>
2017-03-09 21:35:34 -0700

    Update test.py

--- dbmgmt/test.py
+++ dbmgmt/test.py
@@ -4,7 +4,7 @@ conn = psycopg2.connect(database="travel_info", user="postgres", password="pass1
 
 cur = conn.cursor()
 
-cur.execute("INSERT INTO Twitter_Data (ts, tweet, location, country, language, coordinates, threat_type) VALUES ( now(), %s, %s, %s, %s, %s, %s)", (tweet, location, country, language, coords, '0'))
+cur.execute("INSERT INTO Twitter_Data (ts, tweet, location, country, language, coordinates, threat_type) VALUES ( now(), tweet, location, country, language, [40,32], 0))
 
 conn.commit()
 conn.close()
