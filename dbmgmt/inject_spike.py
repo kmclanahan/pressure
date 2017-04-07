e86f292be2c96e38f4bd9a55523737c695b21b4b
kmclanahan <kmclanahan@gmail.com>
2017-04-07 23:38:00 +0000

    update

--- dbmgmt/inject_spike.py
+++ dbmgmt/inject_spike.py
@@ -6,16 +6,15 @@ fake_data = ["Trump", "Trump blows", "Trump blows goats", "Trump blows goats", "
                "Trump blows goats", "Trump blows goats", "Trump blows goats", "Trump blows goats",
                "Trump blows goats", "Trump blows goats", "Trump blows goats", "Trump blows goats",
                "Trump blows goats", "Trump blows goats", "Trump blows goats", "Trump blows goats",
-               "Trump blows goats", "Trump blows goats", "Trump blows goats", "Trump blows goats",
-               "Trump blows goats", "Trump blows goats", "Trump blows goats", "Trump blows goats",
                "Trump blows goats #fakenews", "Trump blows goats #fakenews", "Trump blows goats #fakenews",
                "Trump blows goats #fakenews", "Trump blows goats #fakenews", "Trump blows goats #fakenews",
-               "Trump blows goats #fakenews", "Trump blows goats #fakenews", "Trump blows goats #fakenews"
+               "Trump blows goats #fakenews", "Trump blows goats #fakenews", "Trump blows goats #fakenews",
               ]
 
-longitude = -13.994795
-latitude = 33.754758
-coords = [[-13.994795, 33.754758], [-13.994795, 33.754758], [-13.994795, 33.754758], [-13.994795, 33.754758]]
+longitude = 33.754758
+latitude = -13.994795
+#coords = [[-13.994795, 33.754758], [-13.994795, 33.754758], [-13.994795, 33.754758], [-13.994795, 33.754758]]
+coords = [[33.754758, -13.994795], [33.754758, -13.994795], [33.754758, -13.994795], [33.754758, -13.994795]]
 
 conn = psycopg2.connect(database="travel_info", user="postgres", password="pass1234", host="localhost", port="5432")
 cur = conn.cursor()
