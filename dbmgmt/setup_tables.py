ac6ec15a1554b5c544e44db7ecee3edbd7585513
kmclanahan <kmclanahan@gmail.com>
2017-04-10 04:05:50 +0000

    update

--- dbmgmt/setup_tables.py
+++ dbmgmt/setup_tables.py
@@ -9,16 +9,6 @@ cur.connection.set_isolation_level(0)
 conn = psycopg2.connect(dbname="travel_info", user="postgres", password="pass1234", host="localhost", port="5432")
 cur = conn.cursor()
 
-try:
-    cur.execute("CREATE TABLE Tweet_Spike_Data")
-    cur.execute("CREATE TABLE Tweet_Spike_Text")
-except:
-    cur.execute("DROP TABLE Tweet_Spike_Data")
-    cur.execute("DROP TABLE Tweet_Spike_Text")
-    cur.execute("CREATE TABLE Tweet_Spike_Data")
-    cur.execute("CREATE TABLE Tweet_Spike_Text")
-
-
 cur.execute("CREATE TABLE Tweet_Spike_Data(idx integer, ts timestamp, grid_idx varchar, baseline_level float, current_level float, spike_percent float, top_words varchar, top_hashes varchar, latitude float, longitude float)")
 
 cur.execute("CREATE TABLE Tweet_Spike_Text(id serial PRIMARY KEY, ts timestamp, spike_idx integer, tweet varchar, latitude float, longitude float)")
